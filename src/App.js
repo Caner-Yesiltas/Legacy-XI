5b3b77e439d968b0046cdf0c1ca34b09b104ee5a
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-16 11:58:39 -0500

    add mock data in helper folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Container } from 'react-bootstrap';
 import './styles/App.css';
 import Header from './components/header/Header';
+import LegendContainer from './components/legends/LegendContainer';
 
 function App() {
   return (
   <Container className='text-center my-5' >
-<Header />
+    <Header />
+    <LegendContainer/>
+
+
   </Container>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import logo from '../../assets/logo.png';
 import headerStyle from "./Header.module.css"
 
 const Header = () => {
   return (
-    <div>
-      <Image src={logo} width={'12.5rem'} />
-      <h1 className={headerStyle.title} >Football Legends</h1>
-    </div>
+    <Container>  <div>
+    <Image src={logo} width={"200px"} /> <br></br>
+    <h1 className={`${headerStyle.title} my-2 `} >Football Legends</h1>
+  </div> 
+  </Container>
+  
   );
 };
 
