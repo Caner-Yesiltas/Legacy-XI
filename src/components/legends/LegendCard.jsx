c83f398883fdc42172a0f0c8582c82bcfb4963b6
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-17 11:43:38 -0500

    add legendcard component

---
+++ src/components/legends/LegendCard.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import {Col, Card} from 'react-bootstrap';
+
+const LegendCard = ({legend}) => {
+    const [show, setShow] = useState(true)
+  return (
+    
+      <Col xs={10} sm={8} md={6} lg={4} xl={3} className='g-3'>
+    <Card >
+        {
+            show ? <Card.Img onClick={()=> setShow(false)} variant="top" src={legend.img} className="legend-image"   /> : <> <Card.Body onClick={()=> setShow(true)} >
+            <Card.Title>{legend.name}</Card.Title>
+           <ul className='m-auto w-100 '  > 
+            {legend.statistics.map(item => <li className='list-unstyled h5 text-start' >⚽{item}</li>)}
+           </ul>
+           
+          </Card.Body   >
+          <Card.Footer>
+            Career Years: {legend.official_career}
+          </Card.Footer></> 
+        }
+
+      
+      
+    </Card></Col>
+    
+  )
+}
+
+export default LegendCard
--- src/components/legends/LegendContainer.jsx
+++ src/components/legends/LegendContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Card} from 'react-bootstrap';
 import { data } from '../../helper/data';
+import LegendCard from './LegendCard';
 
 const LegendContainer = () => {
     const [show, setShow] = useState(true)
@@ -13,25 +14,12 @@ const LegendContainer = () => {
 // ❌ Avoid inside component body (causes multiple renders)
   return <Container>
   <Row>
-    {data.map(legend=> 
-    <Col xs={10} sm={8} md={6} lg={4} xl={3} className='g-3'  >
-    <Card >
-        {
-            show ? <Card.Img onClick={()=> setShow(false)} variant="top" src={legend.img} className="legend-image"   /> : <> <Card.Body onClick={()=> setShow(true)} >
-            <Card.Title>{legend.name}</Card.Title>
-           <ul className='m-auto w-100 '  > 
-            {legend.statistics.map(item => <li className='list-unstyled h5 text-start' >⚽{item}</li>)}
-           </ul>
-           
-          </Card.Body   >
-          <Card.Footer>
-            Career Years: {legend.official_career}
-          </Card.Footer></> 
-        }
-
+    {data.map((legend) => 
+        
+        <LegendCard key={legend.id}  legend={legend}  />
       
-      
-    </Card></Col>  )  }
+    )}
+     
   </Row>
     
   </Container>;
